9e505f997fb437791645b69eb64c5faf75a19dd1
Ashish Prajapati <ashish.prajapati@devitpl.com>
2024-08-01 11:29:03 +0530

    resolved path issue

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import "./global.css";
 // interface LayoutProps{
 //   children: ReactNode
 // }
-import NavBar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 import { orbitron } from "./font";
 
 // export const metadata = {
